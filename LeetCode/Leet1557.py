30f6cff863e9f1505ce313197cb9eeea02cf050b
Han Liu <39892087+hliunanoeng@users.noreply.github.com>
2021-06-05 15:18:50 -0400

    Delete LeetCode directory

--- LeetCode/Leet1557.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        dest_set = set()
-        for i in edges:
-            dest_set.add(i[1])
-
-        iso_set = set(list(range(n))).difference(dest_set)
-        return list(iso_set)
